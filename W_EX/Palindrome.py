f47a7f343fa322ed8d1c8926b9513aed16dc0978
VioletEvergardenZz <1561862432@qq.com>
2024-05-09 11:55:21 +0800

    palindrome

--- W_EX/Palindrome.py
+++ W_EX/Palindrome.py
@@ -1,5 +1,4 @@
-def is_palindrome(str):
-    left,right=0,len(str)-1
+def is_palindrome(str,left,right):
     while left<right:
         if str[left]!=str[right]:
             return False
@@ -7,8 +6,8 @@ def is_palindrome(str):
         right-=1
     return True
 
-input_str=input('input a str')
-if is_palindrome(input_str):
+input_str=input('input a str\n')
+if is_palindrome(input_str,0,len(input_str)-1):
     print('yes')
 else:
     print('no')
