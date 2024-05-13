0da62f02156216b0caaedaa36a79fcfde0b64b22
VioletEvergardenZz <1561862432@qq.com>
2024-05-13 19:03:44 +0800

    验证栈序列

--- leetcode/模拟/验证栈序列.py
+++ leetcode/模拟/验证栈序列.py
@@ -1,3 +1,8 @@
+'''
+给定 pushed 和 popped 两个序列，每个序列中的 值都不重复，
+只有当它们可能是在最初空栈上进行的推入 push 和弹出 pop 操作序列的结果时，
+返回 true:否则,返回 false
+'''
 class Solution(object):
     def validateStackSequences(self,pushed:list[int],poped:list[int]):
         stack,i=[],0
