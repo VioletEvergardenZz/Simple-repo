3ad48ddb86bc8848e8499e2193f28bb46552b081
VioletEvergardenZz <1561862432@qq.com>
2024-05-25 18:45:26 +0800

    寻找数组的中心下标

--- leetcode/查找/寻找数组的中心下标.py
+++ leetcode/查找/寻找数组的中心下标.py
@@ -3,9 +3,9 @@
 class Solution:
     def pivotIndex(self, nums: list[int]) -> int:
         sum_left,sum_right=0,sum(nums)
-        for i in range(len(nums)):
+        for i in range(len(nums)):      # 每次循环都是为了判断i是否为中心下标
             sum_right-=nums[i]
-            if sum_left==sum_right:
-                return i
+            if sum_left==sum_right:     # 第一次判断sum_left为0
+                return i                # 返回中心下标
             sum_left+=nums[i]
         return -1
