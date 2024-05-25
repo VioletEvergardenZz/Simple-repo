13576d0c81ef0d0d39ed31ef2b07a7a44c3274b6
VioletEvergardenZz <1561862432@qq.com>
2024-05-25 17:30:07 +0800

    二分查找

--- leetcode/查找/二分查找.py
+++ leetcode/查找/二分查找.py
@@ -4,17 +4,27 @@
 如果目标值存在返回下标,否则返回 -1
 '''
 
+'''
+循环条件 left<right 区间 [left,right)   left和right 指向同一个位置时,循环会停止.无法检查 right 指向的元素，可能会错过目标元素，特别是当目标元素恰好是数组的最后一个元素时。
+        left <= right  [left,right] 
+'''
+
+
 class Solution:
     def search(self, nums:list[int], target: int) -> int:
-        left,right=0,len(nums)-1
-        while left<=right:
-            m=(left+right)//2
+        left,right=0,len(nums)-1      #搜索区间的左右边界
+        while left<=right:      #[left,right]   
+            m=(left+right)//2   #中值索引
+
+            # 中值与target比较大小
             if nums[m]<target:
                 left=m+1
             elif nums[m]>target:
                 right=m-1
             else:
                 return m
+        
+        # target不存在
         return -1
     
 
