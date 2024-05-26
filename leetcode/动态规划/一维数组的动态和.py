83e7b804c98449c308751ed1c2c94b7abd40cf1f
VioletEvergardenZz <1561862432@qq.com>
2024-05-26 14:45:37 +0800

    一维数组的动态和

--- leetcode/动态规划/一维数组的动态和.py
+++ leetcode/动态规划/一维数组的动态和.py
@@ -1,3 +1,14 @@
+'''
+输入:nums = [1,1,1,1,1]
+输出:[1,2,3,4,5]
+解释:动态和计算过程为 [1, 1+1, 1+1+1, 1+1+1+1, 1+1+1+1+1] 。
+'''
+
+'''
+初始状态: f(0)=num[0]
+转移方程: f(i)=f(i-1)+num[i]
+'''
+
 class Solution:
     def runningSum(self, nums: list[int]) -> list[int]:
         dp=[0]*len(nums)
