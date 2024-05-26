ad90512d792f4e2ec0711028b402b56e1dcaddd1
VioletEvergardenZz <1561862432@qq.com>
2024-05-26 15:32:58 +0800

    二叉树的最大深度

--- leetcode/回溯/二叉树的最大深度.py
+++ leetcode/回溯/二叉树的最大深度.py
@@ -11,5 +11,14 @@ class TreeNode:
         
 class Solution:
     def maxDepth(self, root: TreeNode) -> int:
+        '终止条件: root为空时 返回深度0'
         if not root:return 0
+        
+        '''
+        递推
+        计算节点root的左子树深度 调用maxDepth(root.left)
+        计算节点root的右子树深度 调用maxDepth(root.right)
+
+        返回值: 树的深度
+        '''
         return max(self.maxDepth(root.left),self.maxDepth(root.right))+1
