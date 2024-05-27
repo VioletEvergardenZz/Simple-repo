5f18e8bd37ce843f43abc1b8abfceaa251ba3cf7
VioletEvergardenZz <1561862432@qq.com>
2024-05-27 20:08:54 +0800

    旋转字符串

--- leetcode/模拟/旋转字符串.py
+++ leetcode/模拟/旋转字符串.py
@@ -1,5 +1,8 @@
 # 给定两个字符串, s 和 goal。如果在若干次旋转操作之后，s 能变成 goal ，那么返回 true 。
+'s 的 旋转操作 就是将 s 最左边的字符移动到最右边。 '
+
+
 
 class Solution(object):
     def rotateString(self,s,goal):
-        return len(s)==len(goal) and s in (goal+goal)
+        return len(s)==len(goal) and s in (goal+goal)   # 拼接字符串goal goal 中包含原字符串 s
