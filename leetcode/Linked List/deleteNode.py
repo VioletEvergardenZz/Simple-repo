b40d4744818bee5d789f7b0c9c7f3984d79763b5
VioletEvergardenZz <1561862432@qq.com>
2024-05-29 21:32:32 +0800

    删除链表中的节点

--- leetcode/Linked List/deleteNode.py
+++ leetcode/Linked List/deleteNode.py
@@ -1,3 +1,10 @@
+'''
+为了删除节点 node ，可使用以下方法：
+
+复制后继节点 node.next 的「节点值」至节点 node 
+将 node.next 从链表中删除
+'''
+
 class ListNode:
     def __init__(self,val):
         self.val=val
@@ -6,6 +13,6 @@ class ListNode:
 class Solution:
     def deleteNode(self,node:ListNode):
         # node是要删除的节点
-        node.val=node.next.val
+        node.val=node.next.val       
         node.next=node.next.next
     
