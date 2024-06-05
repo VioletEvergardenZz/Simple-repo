2b02f2120b439afacd7c0e9a61ea2811eef36a4e
VioletEvergardenZz <1561862432@qq.com>
2024-06-05 19:16:57 +0800

    判断子序列

--- leetcode/Double pointer/is_subsequence.py
+++ leetcode/Double pointer/is_subsequence.py
@@ -3,6 +3,7 @@ class Solution(object):
     def isSubsequence(self,s,t):
         n,m=len(s),len(t)
         i=j=0
+        # 双指针循环判断
         while i<n and j<m:
             if s[i]==t[j]:
                 i+=1
