c9fb0702aace768705f44c76c29c2670ea6e71da
VioletEvergardenZz <1561862432@qq.com>
2024-07-23 12:24:06 +0800

    约束练习

--- SQL/lianxi.sql.sql
+++ SQL/lianxi.sql.sql
@@ -142,9 +142,9 @@ SELECT d.department_name,d.location_id,COUNT(employee_id),AVG(salary) FROM depar
 SELECT last_name,salary 
 FROM employees
 WHERE salary > (
-                 SELECT salary
-				 FROM employees
-				 WHERE last_name = 'Abel'
+                SELECT salary
+				FROM employees
+				WHERE last_name = 'Abel'
 				);
 
 # 单行子查询 
@@ -152,8 +152,8 @@ WHERE salary > (
 SELECT last_name, job_id, salary
 FROM employees
 WHERE salary =(
-				SELECT MIN(salary)
-				FROM employees
+			   SELECT MIN(salary)
+			   FROM employees
 			   );
 
 # 题目:查询与141号或174号员工的manager_id和department_id相同的其他员工的employee_id,manager_id,department_id
@@ -192,7 +192,7 @@ AND salary < ANY(
 				 SELECT salary
 				 FROM employees
 				 WHERE job_id = 'IT_PROG'
-				);
+				 );
 
 # 题目:查询平均工资最低的部门id
 
@@ -203,7 +203,7 @@ HAVING AVG(salary) <= ALL(
 						  SELECT AVG(salary)
 						  FROM employees
 						  GROUP BY department_id
-						 );
+						  );
 # 空值问题
 SELECT last_name
 FROM employees
@@ -229,7 +229,7 @@ FROM employees e,(
 				  SELECT department_id,AVG(salary) avg_sal
 				  FROM employees
 			      GROUP BY department_id
-				) t_dept_avg_sal
+				  ) t_dept_avg_sal
 WHERE e.department_id = t_dept_avg_sal.department_id
 AND e.salary > t_dept_avg_sal.avg_sal;
 
@@ -243,7 +243,7 @@ ORDER BY(
 		);
 
 # 题目:若employees表中employee_id与job_history表中employee_id相同的数目不小于2,
-#       输出这些相同id的员工的employee_id,last_name和其job_id
+#      输出这些相同id的员工的employee_id,last_name和其job_id
 SELECT employee_id,last_name,job_id
 FROM employees e
 WHERE 2 <= (
@@ -280,21 +280,21 @@ WHERE e.department_id is NULL;
 SELECT department_id,department_name
 FROM departments d
 WHERE NOT EXISTS(
-				SELECT *
-				FROM employees e
- 				WHERE e.department_id = d.department_id
-				);
+				 SELECT *
+			  	 FROM employees e
+ 				 WHERE e.department_id = d.department_id
+				 );
 
 
 
   
 SELECT DATABASE();
 CREATE TABLE IF NOT EXISTS emp1(
-id INT,
-`name` VARCHAR(15),
-hire_date DATE,
-salary DOUBLE(10,2)
-);
+								id INT,
+								`name` VARCHAR(15),
+								hire_date DATE,
+								salary DOUBLE(10,2)
+								);
 DESC emp1;
 
 # 同时插入多条记录
@@ -318,6 +318,110 @@ b INT,
 c INT GENERATED ALWAYS AS (a + b) VIRTUAL
 );
 
+DROP TABLE IF EXISTS emp1;
+
+# 2024.7.23 
+# 约束练习
+# 练习1:已经存在数据库test04_emp,两张表emp2和dept2
+CREATE DATABASE test04_emp;
+use test04_emp;
+CREATE TABLE emp2(
+				  id INT,
+				  emp_name VARCHAR(15)
+				  );
+CREATE TABLE dept2(
+				   id INT,
+				   dept_name VARCHAR(15)
+				   );
+
+SELECT DATABASE();
+SHOW TABLES;
+DESC dept2;
+
+# ADD 用于添加新列或约束（如主键、外键等）,而 MODIFY 用于更改现有列的定义（如数据类型,默认值等）
+#1.向表emp2的id列中添加PRIMARY KEY约束
+ALTER TABLE emp2
+ADD CONSTRAINT pk_emp2_id PRIMARY KEY(id);
+
+#2.向表dept2的id列中添加PRIMARY KEY约束
+ALTER TABLE dept2
+ADD PRIMARY KEY(id);
+
+#3.向表emp2中添加列dept_id,并在其中定义FOREIGN KEY约束,与之相关联的列是dept2表中的id列.
+ALTER TABLE emp2
+ADD dept_id INT;
+ALTER TABLE emp2
+ADD CONSTRAINT fk_emp2_deptid FOREIGN KEY(dept_id) REFERENCES dept2(id);
+
+
+#练习2 承接《第11章_数据处理之增删改》的综合案例.
+# 1、创建数据库test01_library
+CREATE DATABASE IF NOT EXISTS test01_library CHARACTER SET 'utf8';
+USE test01_library;
+
+# 2、创建表 books,表结构如下：
+CREATE TABLE books(
+				   id INT,
+				   name VARCHAR(50),
+				   `authors` VARCHAR(100) ,
+				   price FLOAT,
+			       pubdate YEAR ,
+				   note VARCHAR(100),
+				   num INT
+				   );
+									 
+# 3、使用ALTER语句给books按如下要求增加相应的约束
+# 给id增加主键约束
+ALTER TABLE books ADD PRIMARY KEY(id);
+#给id字段增加自增约束
+ALTER TABLE books MODIFY id INT AUTO_INCREMENT;
+#给name等字段增加非空约束
+ALTER TABLE books MODIFY NAME VARCHAR(50) NOT NULL;
+ALTER TABLE books MODIFY `authors` VARCHAR(100) NOT NULL;
+ALTER TABLE books MODIFY price FLOAT NOT NULL;
+ALTER TABLE books MODIFY pubdate DATE NOT NULL;
+DESC books;
+
+# 练习3 题目:
+#1. 创建数据库test04_company
+CREATE DATABASE test04_company;
+
+#2. 按照下表给出的表结构在test04_company数据库中创建两个数据表offices和employees
+USE test04_company;
+CREATE TABLE offices(
+					 officeCode INT(10),
+					 city VARCHAR(50) NOT NULL,
+					 address VARCHAR(50),
+					 country VARCHAR(50) NOT NULL,
+					 postalCode VARCHAR(15) UNIQUE,
+					 PRIMARY KEY(officeCode)
+					 );
+CREATE TABLE employees(
+					   employeeNumber INT(11) PRIMARY KEY AUTO_INCREMENT,
+					   lastName VARCHAR(50) NOT NULL,
+					   firstName VARCHAR(50) NOT NULL,
+					   mobile VARCHAR(25) UNIQUE,
+					   officeCode INT(10) NOT NULL,
+					   jobTitle VARCHAR(50) NOT NULL,
+					   birth DATETIME NOT NULL,
+					   note VARCHAR(255),
+					   sex VARCHAR(5),
+					   CONSTRAINT fk_emp_ofCode FOREIGN KEY(officeCode) REFERENCES offices(officeCode)
+					   );
+#3. 将表employees的mobile字段修改到officeCode字段后面
+ALTER TABLE employees MODIFY mobile VARCHAR(25) AFTER officeCode;
+#4. 将表employees的birth字段改名为employee_birth
+ALTER TABLE employees CHANGE birth employee_birth DATETIME;
+#5. 修改sex字段,数据类型为CHAR(1),非空约束
+ALTER TABLE employees MODIFY sex CHAR(1) NOT NULL;
+#6. 删除字段note
+ALTER TABLE employees DROP COLUMN note;
+#7. 增加字段名favoriate_activity,数据类型为VARCHAR(100)
+ALTER TABLE employees ADD favoriate_activity VARCHAR(100);
+#8. 将表employees名称修改为employees_info
+ALTER TABLE employees RENAME employees_info;  # RENAME TABLE employees TO employees_info;
+
+DESC employees_info;
 
 
 
